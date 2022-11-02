348463aadc223c9d7c32077c7ca35cb8d991b918
jordanrings <jordanringsakabe@gmail.com>
2022-11-02 10:46:32 -0600

    added event handler to the events surrounding the installation of the PWA

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -26,7 +26,7 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
+// TODO: Implement asset caching✅
 registerRoute(
   ({ requrest }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// TODO: Add logic to a method that accepts some content and adds it to the database　✅
 export const putDb = async (content) =>{
   console.log('PUT to the database');
   const todosDb = await openDB('todos', 1);
@@ -24,7 +24,7 @@ export const putDb = async (content) =>{
 };
 
 
-// TODO: Add logic for a method that gets all the content from the database
+// TODO: Add logic for a method that gets all the content from the database ✅
 export const getDb = async () => {
   console.log('GET all from the database');
   const todosDb = await openDB('todos', 1);
